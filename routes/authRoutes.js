38c7d6e60ccf2332dae35f193d8fad43a6122d29
Naman <njain4282@gmail.com>
2026-03-10 18:05:27 +0530

    fix some

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,7 +12,7 @@ router.post("/register", async (req, res) => {
 
         const { name, email, password } = req.body;
 
-        const user = await User.findOne({ email });
+        let user = await User.findOne({ email });
 
         if (user) {
             res.status(409).json({
@@ -50,7 +50,7 @@ router.post("/login", async (req, res) => {
 
         const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+        let user = await User.findOne({ email });
 
         if (!user) {
             return res.status(400).json({ message: "User not found" });
